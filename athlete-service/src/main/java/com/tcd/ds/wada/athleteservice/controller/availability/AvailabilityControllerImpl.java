e0bcf4edfe58bbd96a190ba195fb184e078dcbb6
Akul Rastogi <akulrastogi92@gmail.com>
2022-03-30 20:19:40 +0100

    Added annotations

--- athlete-service/src/main/java/com/tcd/ds/wada/athleteservice/controller/availability/AvailabilityControllerImpl.java
+++ athlete-service/src/main/java/com/tcd/ds/wada/athleteservice/controller/availability/AvailabilityControllerImpl.java
@@ -5,31 +5,35 @@ import com.tcd.ds.wada.athleteservice.model.AvailabilityRequest;
 import com.tcd.ds.wada.athleteservice.service.AvailabilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class AvailabilityControllerImpl implements AvailabilityController {
 
     @Autowired
     AvailabilityService service;
 
     @Override
-    public ResponseEntity<List<Availability>> get(String athleteId) {
+    public ResponseEntity<List<Availability>> get(Integer athleteId) {
         return service.get(athleteId);
     }
 
     @Override
-    public ResponseEntity<String> add(AvailabilityRequest request) {
+    public ResponseEntity<Integer> add(AvailabilityRequest request) {
         return service.add(request);
     }
 
     @Override
-    public ResponseEntity<?> update(String availabilityId, AvailabilityRequest request) {
+    public ResponseEntity<?> update(Integer availabilityId, AvailabilityRequest request) {
         return service.update(availabilityId, request);
     }
 
     @Override
-    public ResponseEntity<?> delete(String availabilityId) {
+    public ResponseEntity<?> delete(Integer availabilityId) {
         return service.delete(availabilityId);
     }
 
--- athlete-service/src/main/java/com/tcd/ds/wada/athleteservice/repository/AvailabilityRepository.java
+++ athlete-service/src/main/java/com/tcd/ds/wada/athleteservice/repository/AvailabilityRepository.java
@@ -1,13 +1,13 @@
 package com.tcd.ds.wada.athleteservice.repository;
 
 import com.tcd.ds.wada.athleteservice.entity.Availability;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AvailabilityRepository extends CrudRepository<Availability, String> {
-    Optional<List<Availability>> findByAthleteId(String athleteId);
-    Optional<List<Availability>> findByAthleteIdAndIsAppointment(String athleteId, Boolean isAppointment);
+@Repository
+public interface AvailabilityRepository extends CrudRepository<Availability, Integer> {
+    Optional<List<Availability>> findByAthleteId(Integer athleteId);
 }
