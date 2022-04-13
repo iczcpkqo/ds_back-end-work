e9db5027cc04be01b82146c84b99d5bd37a7d0e0
Akul Rastogi <akulrastogi92@gmail.com>
2022-04-13 02:49:58 +0100

    Changed adoId to string

--- ado-service/src/main/java/com/tcd/ds/wada/adoservice/repository/AdoRepository.java
+++ ado-service/src/main/java/com/tcd/ds/wada/adoservice/repository/AdoRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface AdoRepository extends MongoRepository<Ado, Integer> {
+public interface AdoRepository extends MongoRepository<Ado, String> {
     List<Athlete> findByLocation(String country);
 }
